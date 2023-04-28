1400300cf27b9359c89f3e0484386c78a06e352f
bekturali <bekturaliorozov@gmail.com>
2023-04-28 17:51:20 +0600

    Samthing commited

--- FinalAssintment/FinalAssintment/settings.py
+++ FinalAssintment/FinalAssintment/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+TEMPLATE_DIR=os.path.join(BASE_DIR,"Templates")
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -54,7 +56,9 @@ ROOT_URLCONF = 'FinalAssintment.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            TEMPLATE_DIR
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIR=[
+    os.path.join(BASE_DIR,"static")
+]
